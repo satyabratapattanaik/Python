deb63bbf3df84a9de4c22b0b9200df0bd590ca29
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-02 23:30:52 +0530

    Add files via upload

---
+++ takingInput.py
@@ -0,0 +1,12 @@
+#Taking input from user and printing input
+
+name = input("name : ")
+print(name)
+
+age = int(input("age : "))
+print(age)
+
+price = float(input("price : "))
+print(price)
+
+print("My name is ", name, "and I am ", age, "years old.")
