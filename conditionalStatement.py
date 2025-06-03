27434bc50a125cbf9d104838e17f9953546cf593
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-03 09:16:09 +0530

    Add files via upload

---
+++ conditionalStatement.py
@@ -0,0 +1,10 @@
+light = input("Light : ")
+
+if(light == "red"):
+    print("STOP")
+elif(light == "yellow"):
+    print("LOOK")
+elif(light == "green"):
+    print("GO")
+else:
+    print("Light is broken")
