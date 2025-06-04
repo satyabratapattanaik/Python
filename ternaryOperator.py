f6e1b5b7c99cb877e3b1cb16dc87154bd9711a38
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-04 09:07:13 +0530

    Update ternaryOperator.py

--- ternaryOperator.py
+++ ternaryOperator.py
@@ -3,3 +3,7 @@ eat = "Yes" if food == "Cake" else "No"
 print(eat)
 
 print("Sweet") if food == "Cake" or food == "Jalebi" else print("not sweet")
+
+age = int(input("age : "))
+vote = ("Yes", "No") [age<18]
+print(vote)
