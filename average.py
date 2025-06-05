d0c9e537ce84e99fee757c2b3d1a54946044979b
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-05 08:29:17 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+# WAP to input 2 floating point numbers and print their average
+
+num1 = float(input("num1 : "))
+num2 = float(input("num2 : "))
+avg = (num1 + num2)/2
+
+print("The average is : ", avg)
