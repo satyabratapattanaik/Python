4d5b0e484ca4cd06a68df9775d2a527588e60c5f
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-05 08:45:38 +0530

    Add files via upload

---
+++ trueFalse.py
@@ -0,0 +1,9 @@
+""" WAP to input 2 int numbers a and b.
+    Print True if a is greater than or equal to b. If not print False """
+
+a = int(input("a : "))
+b = int(input("b : "))
+if(a >= b):
+    print("True")
+else:
+    print("False")       
