1c55123409e2efc217fc3d79b765e96f249303fb
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-07 16:03:15 +0530

    Add files via upload

---
+++ addElementsIntoList.py
@@ -0,0 +1,12 @@
+# WAP to input 3 movies and store them in a List
+
+movies = []
+mov1 = input("Movie1 : ")
+mov2 = input("Movie2 : ")
+mov3 = input("Movie3 : ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
