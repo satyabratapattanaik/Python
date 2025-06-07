fec7d3a38a6b310483a0680c0a248d9254563553
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-07 15:22:28 +0530

    Add files via upload

---
+++ listIndexing&Slicing.py
@@ -0,0 +1,11 @@
+# List Indexing and Slicing
+
+marks = [87, 64, 33, 95, 76]
+
+print(marks)
+print(marks[0])
+print(marks[1:4])
+print(marks[:4])
+print(marks[1:])
+print(marks[1:len(marks)])
+print(marks[-3:-1])
