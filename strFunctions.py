7955c7b5ed5b01d1c96548276645a8686c86d006
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-07 12:46:08 +0530

    Add files via upload

---
+++ strFunctions.py
@@ -0,0 +1,13 @@
+# String Functions
+
+str = "I am studying python from ApnaCollege"
+
+print(str.endswith("ege")) 
+print(str.endswith("app"))
+
+strr = "i am studying python from ApnaCollege"
+
+print(strr.capitalize())
+print(strr.replace("o","a"))
+print(strr.find("o"))
+print(strr.count("from"))
