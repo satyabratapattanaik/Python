d9ac72de289647f2cd494e7d7729ce350a28f22c
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-08 12:07:29 +0530

    Add files via upload

---
+++ nestedDictionary.py
@@ -0,0 +1,14 @@
+# Nested Dictionary
+
+student = {
+    "name" : "Satya",
+    "subjects" : {
+        "maths" : 100,
+        "physics" : 88,
+        "chemistry" : 90
+    }
+}
+
+print(student)
+print(student["subjects"])
+print(student["subjects"]["maths"])
