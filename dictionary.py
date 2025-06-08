3c170b73236899753303daf2b99c248103601d10
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-08 11:46:42 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# Dictionary Definition
+
+info = {
+    "name" : "Satya",
+    "age" : 24,
+    "gender" : "Male",
+    "subject" : ["Python", "C++", "Java"],
+    "tools" : ("MS excel", "Power BI", "Tableau"),
+    12 :12.0
+}
+
+print(info)
+print(type(info))
