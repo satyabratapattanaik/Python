93c148ba568f6c6beca4184ee7c5b6cd731c2708
Satyabrata Pattanaik <89653496+satyabratapattanaik@users.noreply.github.com>
2025-06-09 08:43:44 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,21 @@
+# Function Definition
+
+def Calc_sum(a, b):
+    sum = a+b
+    print(sum)
+    return sum
+
+Calc_sum(12, 17)
+
+
+def print_hello():
+    print("Hello")
+    
+print_hello()
+
+def avg(a, b, c):
+    avg = (a + b + c)/3
+    print(avg)
+    return(avg)
+
+avg(12, 24, 14)
